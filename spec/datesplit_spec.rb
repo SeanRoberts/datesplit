82afb4c6886e168ed7534f3585db49d739e96aa4
Michael Guterl <michael@diminishing.org>
2011-09-14 18:03:55 -0400

    remove unnecessary Date#to_s

--- spec/datesplit_spec.rb
+++ spec/datesplit_spec.rb
@@ -3,44 +3,44 @@ require 'spec_helper'
 describe Datesplit do
   it "handles a format of '<Month> <Day> to <Month> <Day>, <Year>'" do
     d = Datesplit.new('April 11 to April 20, 2012')
-    d.start_date.to_s.should == Date.parse('April 11, 2012').to_s
-    d.end_date.to_s.should == Date.parse('April 20, 2012').to_s
+    d.start_date.should == Date.parse('April 11, 2012')
+    d.end_date.should == Date.parse('April 20, 2012')
   end
 
   it "handles a format of '<Month> <Day> to <Month> <Day>'" do
     d = Datesplit.new('April 11 to April 20')
-    d.start_date.to_s.should == Date.parse('April 11').to_s
-    d.end_date.to_s.should == Date.parse('April 20').to_s
+    d.start_date.should == Date.parse('April 11')
+    d.end_date.should == Date.parse('April 20')
   end
 
   it "handles a format of '<Month> <Day>-<Day> <Year>'" do
     d = Datesplit.new('April 11th-20th 2012')
-    d.start_date.to_s.should == Date.parse('April 11 2012').to_s
-    d.end_date.to_s.should == Date.parse('April 20 2012').to_s
+    d.start_date.should == Date.parse('April 11 2012')
+    d.end_date.should == Date.parse('April 20 2012')
   end
 
   it "handles a format of '<Month> <Day>-<Day>'" do
     d = Datesplit.new('April 11th-20th')
-    d.start_date.to_s.should == Date.parse('April 11').to_s
-    d.end_date.to_s.should == Date.parse('April 20').to_s
+    d.start_date.should == Date.parse('April 11')
+    d.end_date.should == Date.parse('April 20')
   end
 
   it "handles a format of '<Month> <Day> to <Day> <Year>'" do
     d = Datesplit.new('April 11 to 20 2012')
-    d.start_date.to_s.should == Date.parse('April 11 2012').to_s
-    d.end_date.to_s.should == Date.parse('April 20 2012').to_s
+    d.start_date.should == Date.parse('April 11 2012')
+    d.end_date.should == Date.parse('April 20 2012')
   end
 
   it "handles a format of '<Month> <Day>-<Month> <Day>, <Year>'" do
     d = Datesplit.new('March 30-April 1, 2012')
-    d.start_date.to_s.should == Date.parse('March 30 2012').to_s
-    d.end_date.to_s.should == Date.parse('April 1 2012').to_s
+    d.start_date.should == Date.parse('March 30 2012')
+    d.end_date.should == Date.parse('April 1 2012')
   end
 
   it "handles a format of '<Month> <Day>, <Year> to <Month> <Day> <Year>'" do
     d = Datesplit.new('April 9, 2012 to January 1st 2013')
-    d.start_date.to_s.should == Date.parse('April 9 2012').to_s
-    d.end_date.to_s.should == Date.parse('January 1 2013').to_s
+    d.start_date.should == Date.parse('April 9 2012')
+    d.end_date.should == Date.parse('January 1 2013')
   end
 
   it "raises a ParseError given an unknown format" do
