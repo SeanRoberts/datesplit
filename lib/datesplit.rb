c0535135d8a047af160efdcfceb0b8e3f0899d52
Michael Guterl <michael@diminishing.org>
2011-09-14 17:40:31 -0400

    raise Datesplit::ParseError when given an unknown format

--- lib/datesplit.rb
+++ lib/datesplit.rb
@@ -1,5 +1,8 @@
 require 'date'
 class Datesplit
+
+  ParseError = Class.new(StandardError)
+
   def initialize(date_string)
     @date_string = String.new(date_string)
   end
@@ -8,36 +11,36 @@ class Datesplit
     expressions.each do |expression, returns|
       m = @date_string.match(expression)
       if m
-        @start_date = Date.parse("#{m.captures[returns.index(:month_only) || returns.index(:month_one)]} #{m.captures[returns.index(:day_one)]} #{m.captures[returns.index(:year_only) || returns.index(:year_one)]}") 
+        @start_date = Date.parse("#{m.captures[returns.index(:month_only) || returns.index(:month_one)]} #{m.captures[returns.index(:day_one)]} #{m.captures[returns.index(:year_only) || returns.index(:year_one)]}")
         break
       end
-    end  
-    raise "Can't process #{@date_string}" unless @start_date 
+    end
+    raise ParseError, "Can't process #{@date_string}" unless @start_date
     return @start_date
   end
-    
+
   def end_date
     expressions.each do |expression, returns|
       m = @date_string.match(expression)
       if m
-        @end_date = Date.parse("#{m.captures[returns.index(:month_only) || returns.index(:month_two) ]} #{m.captures[returns.index(:day_two)]} #{m.captures[returns.index(:year_only) || returns.index(:year_two)]}") 
+        @end_date = Date.parse("#{m.captures[returns.index(:month_only) || returns.index(:month_two) ]} #{m.captures[returns.index(:day_two)]} #{m.captures[returns.index(:year_only) || returns.index(:year_two)]}")
         break
       end
     end
-    raise "Can't process #{@date_string}" unless @end_date 
+    raise ParseError, "Can't process #{@date_string}" unless @end_date
     return @end_date
   end
-  
+
   def number_of_days
     (end_date - start_date).to_i
   end
-  
-  private 
+
+  private
     def expressions
       {
-        
+
         /(\w+)\.*(?:\t|\s)*(\d+)\w*(?:\t|\s)*(?:\-|to)(?:\t|\s)*(\d+)\w*,*(?:\t|\s)*(\d*)/                                            => [:month_only, :day_one, :day_two, :year_only], # September 13-15, 2011 (Year Optional)
-        /(\w+)\.*(?:\t|\s)*(\d+)\w*(?:\t|\s)*(?:\-|to)(?:\t|\s)*(\w+)\.*(?:\t|\s)*(\d+)\w*,*(?:\t|\s)*(\d*)/                          => [:month_one, :day_one, :month_two, :day_two, :year_only], # September 13 to October 15, 2011 (Year Optional)        
+        /(\w+)\.*(?:\t|\s)*(\d+)\w*(?:\t|\s)*(?:\-|to)(?:\t|\s)*(\w+)\.*(?:\t|\s)*(\d+)\w*,*(?:\t|\s)*(\d*)/                          => [:month_one, :day_one, :month_two, :day_two, :year_only], # September 13 to October 15, 2011 (Year Optional)
         /(\w+)\.*(?:\t|\s)*(\d{1,2})\w*,*(?:\t|\s)*(\d{4})(?:\t|\s)*(?:\-|to)(?:\t|\s)*(\w+)\.*(?:\t|\s)*(\d+)\w*,*(?:\t|\s)*(\d{4})/ => [:month_one, :day_one, :year_one, :month_two, :day_two, :year_two] # September 13, 2012 to October 15 2013 (Year Required)
       }
     end
--- spec/datesplit_spec.rb
+++ spec/datesplit_spec.rb
@@ -6,41 +6,46 @@ describe Datesplit do
     d.start_date.to_s.should == Date.parse('April 11, 2012').to_s
     d.end_date.to_s.should == Date.parse('April 20, 2012').to_s
   end
-  
+
   it "handles a format of '<Month> <Day> to <Month> <Day>'" do
     d = Datesplit.new('April 11 to April 20')
     d.start_date.to_s.should == Date.parse('April 11').to_s
     d.end_date.to_s.should == Date.parse('April 20').to_s
   end
-  
+
   it "handles a format of '<Month> <Day>-<Day> <Year>'" do
     d = Datesplit.new('April 11th-20th 2012')
     d.start_date.to_s.should == Date.parse('April 11 2012').to_s
     d.end_date.to_s.should == Date.parse('April 20 2012').to_s
   end
-  
+
   it "handles a format of '<Month> <Day>-<Day>'" do
     d = Datesplit.new('April 11th-20th')
     d.start_date.to_s.should == Date.parse('April 11').to_s
     d.end_date.to_s.should == Date.parse('April 20').to_s
   end
-  
+
   it "handles a format of '<Month> <Day> to <Day> <Year>'" do
     d = Datesplit.new('April 11 to 20 2012')
     d.start_date.to_s.should == Date.parse('April 11 2012').to_s
     d.end_date.to_s.should == Date.parse('April 20 2012').to_s
   end
-  
-  
+
   it "handles a format of '<Month> <Day>-<Month> <Day>, <Year>'" do
     d = Datesplit.new('March 30-April 1, 2012')
     d.start_date.to_s.should == Date.parse('March 30 2012').to_s
     d.end_date.to_s.should == Date.parse('April 1 2012').to_s
   end
-  
+
   it "handles a format of '<Month> <Day>, <Year> to <Month> <Day> <Year>'" do
     d = Datesplit.new('April 9, 2012 to January 1st 2013')
     d.start_date.to_s.should == Date.parse('April 9 2012').to_s
     d.end_date.to_s.should == Date.parse('January 1 2013').to_s
+  end
+
+  it "raises a ParseError given an unknown format" do
+    d = Datesplit.new('garbage data')
+    expect { d.start_date }.to raise_error(Datesplit::ParseError)
+    expect { d.end_date }.to raise_error(Datesplit::ParseError)
   end
 end
